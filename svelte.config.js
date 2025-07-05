9897c02b9cf839c3e75a434499e654d19a353480
avellanamolida <avellana.molida@outlook.com>
2025-07-05 01:12:26 -0500

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import preprocess from 'svelte-preprocess';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
